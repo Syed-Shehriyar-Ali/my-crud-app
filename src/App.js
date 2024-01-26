6da8ec4240b4dfc507b4bd77f7dcf045642ad3d4
Syed-Shehriyar-Ali <154275052+Syed-Shehriyar-Ali@users.noreply.github.com>
2024-01-26 22:48:55 +0500

    My-React-Crud-App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import Header from './Components/Header/index.jsx'
 import './App.css';
+import {
+  BrowserRouter as Router,
+
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/Home/index.jsx'
+import Add from './Components/Add/index.jsx'
+import Edit from './Components/Edit/index.jsx'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+          <Router>  
+                <Routes>
+                      <Route path="/" element={ <Home />}/>
+                       
+                      
+                      <Route path="/Add" element={ <Add />}/>
+                        
+                    
+                      <Route path="/Edit"  element={ <Edit />}/>
+                        
+                    
+                </Routes>
+          </Router>
+    </>
+
   );
 }
 
